45626aae4cdf7c42bd25e7ad4a107c9ba4568f83
justin <justin.lambert@agriculture.gov.au>
2021-09-01 15:55:03 +1000

    packagify

--- entry.js
+++
@@ -1,4 +0,0 @@
-// export { components as intelComponents } from "dist/intel-api";
-// export { ZipCodeValidator as RegExpBasedZipCodeValidator } from "./ZipCodeValidator";
-module.exports = async function getContests() {
-}
