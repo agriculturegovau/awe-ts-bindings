57144e5d0447a803e42675328f9431b03b7760f1
justin <justin.lambert@agriculture.gov.au>
2021-09-01 16:28:57 +1000

    packagify

--- entry.ts
+++ entry.ts
@@ -1,1 +1,2 @@
-export { components } from './dist/profile-api';
+export { components as profileComponents} from './dist/profile-api';
+export { components as intelComponents} from './dist/intel-api';
